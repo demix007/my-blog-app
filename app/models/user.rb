357bf741c98bceddf60f63abd57c372fc6dbaff3
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 19:48:57 +0100

    ADD VALIDATIONS FOR USER MODEL

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,11 @@ class User < ApplicationRecord
     has_many :likes
     has_many :comments
     after_save :most_recent_post
+    validates :name, presence: true
+    validates :posts_counter, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
 
     def most_recent_post
       posts.order(created_at: :desc).includes(:user).limit(3)
     end
+    
 end
