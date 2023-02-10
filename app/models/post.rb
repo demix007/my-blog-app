8671198e067a254858bd9cb25a4cb83e1d9bf596
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 16:03:29 +0100

    ADD update _posts_counter TO Post CLASS

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,13 @@ class Post < ApplicationRecord
     has_many :likes
     has_many :comments
 
+    after_save :update_posts_counter
+
     def most_recent_comment
       comments.where(posts_id: id).order(created_at: :desc).limit(5)
+    end
+  
+    def update_posts_counter
+      user.increment!(:posts_counter)
     end
 end
