b927f4301d368e21880d5966d2958d88d5c29781
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 16:04:12 +0100

    SET UP ASSOCIATIONS BETWEEEN LIKE AND AUTHOR, AND POST

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,11 @@ class Post < ApplicationRecord
 
     after_save :update_posts_counter
 
+    def update_posts_counter
+        user.increment!(:posts_counter)
+    end
+
     def most_recent_comment
       comments.where(posts_id: id).order(created_at: :desc).limit(5)
-    end
-  
-    def update_posts_counter
-      user.increment!(:posts_counter)
     end
 end
