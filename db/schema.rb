483ee46b8c06d45974913f54b8268c4e7cd7696f
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 11:31:22 +0100

    CREATE LIKE SCHEMA

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_102809) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_103026) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
     t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
