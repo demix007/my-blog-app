621450edf6349335d3cb649e7e968bf88dff78d2
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 17:05:52 +0100

    IMPLEMENT OPTIONAL CHANGES REQUIRED BY THE REVIEWER

--- app/models/like.rb
+++ app/models/like.rb
@@ -3,7 +3,9 @@ class Like < ApplicationRecord
     belongs_to :post
     after_save :update_likes_counter
 
+    private
     def update_likes_counter
       post.increment!(:likes_counter)
     end
+
 end
