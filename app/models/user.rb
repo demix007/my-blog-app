8c6be272def7051e39a8eecc25ec9f4629fb7b2d
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 15:59:11 +0100

    ADD most_recent_post METHOD TO USER CLASS

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_many :posts
     has_many :likes
     has_many :comments
+    after_save :most_recent_post
+
+    def most_recent_post
+      posts.order(created_at: :desc).includes(:user).limit(3)
+    end
 end
