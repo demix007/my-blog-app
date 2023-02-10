fbe1087054156526d8849a2b916b620768531e8d
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 16:01:51 +0100

    ADD most_recent_comment METHOD TO Post CLASS

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
     belongs_to :author, class_name: 'User'
     has_many :likes
     has_many :comments
+
+    def most_recent_comment
+      comments.where(posts_id: id).order(created_at: :desc).limit(5)
+    end
 end
