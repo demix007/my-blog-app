a1f9d1fcf2ab1388513e9ffedac6908c69f0c4f8
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 11:38:10 +0100

    ADD users_id AS FOREIGN KEY TO COMMENTS TABLE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_103524) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_103726) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_103524) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_comments_on_users_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -43,4 +45,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_103524) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users", column: "users_id"
 end
