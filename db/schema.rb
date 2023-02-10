3257e52818c813504c34266697b7cda3827ba754
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-10 11:40:02 +0100

    ADD users_id AS FOREIGN KEY TO POST TABLE

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_103831) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_10_103927) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,6 +36,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_103831) do
     t.integer "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "users_id", null: false
+    t.index ["users_id"], name: "index_posts_on_users_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -49,4 +51,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_103831) do
 
   add_foreign_key "comments", "posts", column: "posts_id"
   add_foreign_key "comments", "users", column: "users_id"
+  add_foreign_key "posts", "users", column: "users_id"
 end
