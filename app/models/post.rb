21aef7d2cb2031d7cc2ed1086313ca96e2451f7d
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-11 08:40:04 +0100

    ADD TEST SAMPLES FOR POST MODEL

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
     has_many :comments
     after_save :update_posts_counter
     validates :text, presence: true
-    validates :title, presence: true, length: { in: 3..100 }
+    validates :title, presence: true, length: { in: 3..250 }
     validates :comments_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
     validates :likes_counter, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,36 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  subject { User.new(name: 'Ayodeji', photo: 'Abidemi DP', bio: 'A Full-stack Web Developer', post_counter: 5) }
+  subject { User.new(name: 'Ayodeji', photo: 'Ayodeji Photo URL', bio: 'A Full-stack Web Developer', post_counter: 5) }
 
   before { subject.save }
 
-  it 'Subject is not valid without a name' do
+  it 'expects the user name to be present' do
     subject.name = nil
     expect(subject).to_not be_valid
   end
 
-  it 'The post_counter is only valid whent it is an integer' do
+  it 'expects the user name to not be too long' do
+    subject.name = 'Ayodeji' * 200
+    expect(subject).to_not be_valid 
+  end
+
+  it 'expects the user name to not be too short' do
+    subject.name = 'a'
+    expect(subject).to_not be_valid
+  end
+
+  it 'expects the post_counter to only be valid when it is an integer' do
     subject.post_counter = 'abc'
     expect(subject).to_not be_valid
   end
 
-  it 'name should not be too long' do
-    subject.name = 'abc' * 30
+  it 'expects the bio to not be empty' do
+    subject.bio = ' '
     expect(subject).to_not be_valid
   end
 
-  it 'bio should not be empty' do
-    subject.bio = nil
-    expect(subject).to_not be_valid
+  it 'expects the bio to have a valid value' do
+    expect(subject.bio).to eql 'A Full-stack Web Developer'
   end
 
-  it 'photo should have valid value' do
-    expect(subject.photo).to eql 'Abidemi DP'
+  it 'expect photo to have a valid value' do
+    expect(subject.photo).to eql 'Ayodeji Photo URL'
   end
 
-  it 'The post_counter is expected to be greater than or equal to zero' do
-    subject.post_counter = -10
+  it 'expects the post_counter to be greater than or equal to zero' do
+    subject.post_counter = -1
     expect(subject).to_not be_valid
   end
 
