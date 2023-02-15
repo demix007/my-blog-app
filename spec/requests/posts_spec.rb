dba568e732534c7766fec614251221a61fb5c61b
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-15 12:40:17 +0100

    ADD PATH TO TEST

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'PostsController', type: :request do
+  describe 'GET posts_controller#index' do
+    before(:example) { get '/users/:user_id/posts' } # get(:index)
   end
 end
