d701ec280820f979969929b9c1bf14b3390e7175
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-15 12:42:20 +0100

    ADD TEST CASES FOR THE SHOW ACTION IN THE PostsController

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -18,5 +18,16 @@ RSpec.describe 'PostsController', type: :request do
 
   describe 'GET posts_controller#show' do
     before(:example) { get '/users/:user_id/posts/:post_id' } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders the index body correctly" do
+      expect(response.body).to include('This is used to display post with post_id and user with user_id')
+    end
+
+    it "renders the show template correctly" do
+      expect(response).to render_template(:show)
+    end
   end
 end
