f486fe01763eedfbde93e7729d502491eecf1dd5
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-15 12:51:28 +0100

    ADD TEST CASES FOR THE SHOW ACTION IN THE UsersController

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe 'UsersController', type: :request do
       expect(response).to render_template(:index)
     end
   end
+
+  describe 'GET users_controller#show' do
+    before(:example) { get '/users/:user_id' } # get(:show)
+    it 'is a success' do
+      expect(response).to have_http_status(:ok)
+    end
+
+    it "renders the index body correctly" do
+      expect(response.body).to include('This is used to display user with id')
+    end
+
+    it "renders the index template correctly" do
+      expect(response).to render_template(:show)
+    end
+  end
 end
