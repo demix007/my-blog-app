f65f47bb4c385d495d245820c164b3de1e4588c6
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-15 12:40:57 +0100

    ADD TEST CASES FOR THE INDEX ACTION IN PostsController

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -3,5 +3,16 @@ require 'rails_helper'
 RSpec.describe 'PostsController', type: :request do
   describe 'GET posts_controller#index' do
     before(:example) { get '/users/:user_id/posts' } # get(:index)
+    it 'is a success' do
+      expect(response).to have_http_status(200)
+    end
+
+    it "renders index body correctly" do
+      expect(response.body).to include('This is responsible for showing post with User_id')
+    end
+
+    it "renders the index template correctly" do
+      expect(response).to render_template(:index)
+    end
   end
 end
