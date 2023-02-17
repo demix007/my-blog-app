83266ff85a887b0c9a94a997753987294ba31c56
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-17 23:29:34 +0100

    ADD users#index AS THE ROOT ROUTE

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'users#index'
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show]
   end
