05a4f3f2223fb287bbcd1fec13b51bab71314b46
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-17 23:21:30 +0100

    MODIFY THE show ACTION IN THE UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
-    @id = params[:id]
+    @user = User.find(params[:user_id])
+    @post = @user.posts.find(params[:id])
   end
 end
