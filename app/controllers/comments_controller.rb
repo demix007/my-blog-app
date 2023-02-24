cff8d3766145f3364ed6d77490314b3866f75970
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-24 09:47:35 +0100

    ADD create method to CommentsController

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,18 @@ class CommentsController < ApplicationController
         @post = Post.find(params[:post_id])
         @comment = Comment.new
     end
+
+    def create
+        @post = Post.find(params[:post_id])
+        @comment = Comment.new(comment_params)
+        @comment.author = current_user
+        @comment.post = @post
+    
+        if @comment.save
+          redirect_to user_post_path(user_id: @post.author.id, id: @post.id), notice: 'Comment added successfully'
+        else
+          flash.now[:alert] = @comment.errors.full_messages.first if @comment.errors.any?
+          render :new, status: 400
+        end
+    end
 end
