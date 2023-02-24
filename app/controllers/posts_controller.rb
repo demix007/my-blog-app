80d65531da6633ada86ef132dd0a731ae6d7012a
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-24 09:56:01 +0100

    ADD new METHOD TO PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,8 @@ class PostsController < ApplicationController
     @user = User.find(params[:user_id])
     @post = @user.posts.find(params[:id])
   end
+
+  def new
+    @post = Post.new
+  end
 end
