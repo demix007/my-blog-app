2e9779ae7bba4516cf8238bcf3287b828833bea9
Ayodeji Abidemi <ayodejiabidemi007@gmail.com>
2023-02-24 09:53:38 +0100

    ADD METHOD TO CREATE NEW LIKES

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,9 @@
 class LikesController < ApplicationController
+    def create
+        @like = Like.new
+        @like.author = current_user
+        @like.post = Post.find(params[:posts_id])
+        @like.save!
+        redirect_to user_post_path(user_id: params[:user_id], id: params[:posts_id])
+      end
 end
